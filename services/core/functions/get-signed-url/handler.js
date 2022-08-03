45dbff10ba618bab2482258c9268f86b0141b5bb
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-08-03 12:20:12 +0400

    feat(core): implement s3 get signed url

---
+++ services/core/functions/get-signed-url/handler.js
@@ -0,0 +1,29 @@
+var AWS = require("aws-sdk");
+var s3 = new AWS.S3();
+var bucketName = process.env.AWS_BUCKET_NAME;
+
+exports.main = async (event, context) => {
+  const body = JSON.parse(event.body);
+  if (!body.hasOwnProperty("contentType")) {
+    context.fail({ err: "Missing contentType" });
+  }
+
+  if (!body.hasOwnProperty("filePath")) {
+    context.fail({ err: "Missing filePath" });
+  }
+
+  var params = {
+    Bucket: bucketName,
+    Key: body.filePath,
+    Expires: 3600,
+    ContentType: body.contentType,
+  };
+
+  const url = s3.getSignedUrl("putObjectAcl", params);
+  const responseBody = { url };
+  console.log(responseBody);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(responseBody),
+  };
+};
