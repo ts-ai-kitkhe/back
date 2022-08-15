87719ce10244c741558e0e62f770717cd6f2b902
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-08-16 01:45:08 +0400

    feat(core): specify s3 path by book id

--- services/core/functions/get-signed-url/handler.js
+++ services/core/functions/get-signed-url/handler.js
@@ -1,9 +1,13 @@
+import { getBody } from "../../libs";
+
 var AWS = require("aws-sdk");
 var s3 = new AWS.S3();
 var bucketName = process.env.AWS_BUCKET_NAME;
 
 exports.main = async (event, context) => {
-  const body = JSON.parse(event.body);
+  const body = getBody(event);
+  const bookId = event.pathParameters.id;
+
   if (!body.hasOwnProperty("contentType")) {
     context.fail({ err: "Missing contentType" });
   }
@@ -14,7 +18,7 @@ exports.main = async (event, context) => {
 
   var params = {
     Bucket: bucketName,
-    Key: body.filePath,
+    Key: `books/${bookId}/pages/${body.filePath}`,
     Expires: 3600,
     ContentType: body.contentType,
   };
