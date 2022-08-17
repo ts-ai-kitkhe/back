3e7e1db3e28a3f5ccf053b763413d772c99ee762
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-08-18 01:47:16 +0400

    feat(core): return only public books in get all

--- services/core/functions/books-get-all/handler.js
+++ services/core/functions/books-get-all/handler.js
@@ -4,6 +4,7 @@ export async function main() {
   const bookRepository = new BookRepository();
   let books = await bookRepository.scan();
   books = books.sort((a, b) => b.createdAt - a.createdAt);
+  books = books.filter((book) => book.visibility === "Public");
 
   return {
     statusCode: 200,
--- services/core/functions/books-update/handler.js
+++ services/core/functions/books-update/handler.js
@@ -6,11 +6,11 @@ export async function main(event) {
 
   const bookRepository = new BookRepository();
   const book = await bookRepository.update({
-    Id: bookId,
+    ...(await bookRepository.get({ Id: bookId })),
     title: body.title,
     authorName: body.authorName,
     year: body.year,
-    year: body.visibility,
+    visibility: body.visibility,
   });
 
   return {
