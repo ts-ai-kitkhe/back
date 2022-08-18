97d8a40c7fbcfb48797524ee49e0890f6775e790
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-08-18 23:56:26 +0400

    feat(core): get books added by current admin only

--- services/core/functions/books-get-all-admin/handler.js
+++ services/core/functions/books-get-all-admin/handler.js
@@ -1,9 +1,12 @@
 import { BookRepository } from "../../libs";
 
-export async function main() {
+export async function main(event) {
+  const email = event.requestContext.authorizer.jwt.claims.email;
   const bookRepository = new BookRepository();
   let books = await bookRepository.scan();
+
   books = books.sort((a, b) => b.createdAt - a.createdAt);
+  books = books.filter((book) => book.addedBy === email);
 
   return {
     statusCode: 200,
