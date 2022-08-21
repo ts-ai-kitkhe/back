ea4dde6a79d1c8f7e26fcb1790b34171f12885bc
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-08-21 19:37:34 +0400

    feat(core): add toJSON methods

--- services/core/libs/models/base-model.js
+++ services/core/libs/models/base-model.js
@@ -1,4 +1,11 @@
 export class BaseModel {
   createdAt;
   updatedAt;
+
+  toJSON() {
+    return {
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt,
+    };
+  }
 }
--- services/core/libs/models/book.js
+++ services/core/libs/models/book.js
@@ -8,4 +8,24 @@ export class Book extends BaseModel {
   visibility;
   addedBy;
   coverImagePath;
+
+  get coverImageUrl() {
+    return this.coverImagePath
+      ? `https://assets.ts-ai-kitkhe.ge/${this.coverImagePath}`
+      : undefined;
+  }
+
+  toJSON() {
+    return {
+      ...super.toJSON(),
+      Id: this.Id,
+      title: this.title,
+      authorName: this.authorName,
+      year: this.year,
+      visibility: this.visibility,
+      addedBy: this.addedBy,
+      coverImagePath: this.coverImagePath,
+      coverImageUrl: this.coverImageUrl,
+    };
+  }
 }
