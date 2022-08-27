d36d13973ddbebdb4a9a3ad3bbf78d8edd69fac6
CSopiko <skurd18@freeuni.edu.ge>
2022-08-27 21:49:42 +0400

    feat(ml): update concatenate text

--- services/ml/functions/concatenate-text/handler.py
+++ services/ml/functions/concatenate-text/handler.py
@@ -7,27 +7,43 @@ import json
 TXT_FOLDER_PATH = '/text/'
 ml_bucket_name = os.environ["S3_ML_BUCKET_NAME"]
 
-s3 = boto3.resource("s3")
-
+s3 = boto3.client("s3")
+s3_resource = boto3.resource("s3")
 
 def main(event, context):
-    d = event.get("detail")
-    b = d.get("bucket")
-    bucket = b.get("name")
-    o = d.get("object")
-    key = o.get("key")
-    print(event)
-
-    if not re.match(re.compile(r'\bbooks/.*/pages/text/.*\.txt\b'), key):
-        print(key)
-        return
-
-    if bucket != ml_bucket_name:
-        print(bucket)
-        return
+    # bucket_name = 'books/8462a56f-f641-4b5c-bfb9-c7cf3b751e63/pages/text/0002.txt'
+    bucket_name = event['detail']['bucket']['name']
     
+    # key = 'books/8462a56f-f641-4b5c-bfb9-c7cf3b751e63/pages/text/0002.txt'
+    key = event['detail']['object']['key']
+    
+    if not re.match(re.compile(r'\bbooks/.*/pages/text/.*\.txt\b'), key):
+        print(f"invalid key passed: {key}")
+        return
+
+    if bucket_name != ml_bucket_name:
+        print(f'invalid bucket name, expected:{ml_bucket_name}, got: {bucket_name}')
+        return
+
+    # text_prefix = 'books/8462a56f-f641-4b5c-bfb9-c7cf3b751e63/pages/text'
+    text_prefix = key.rsplit('/', 1)[0]
+
+    # objects = ['books/8462a56f-f641-4b5c-bfb9-c7cf3b751e63/pages/text/1.txt', 'books/8462a56f-f641-4b5c-bfb9-c7cf3b751e63/pages/text/2.txt']
+    objects = s3.list_objects_v2(Bucket=bucket_name,Prefix=text_prefix)
+    print("objects")
+    print(objects)
+
     txt_objects = []
-    for obj in bucket.objects.all():
-        txt_objects.append(json.loads(obj.get()["Body"].read()))
-        print("*"*100, txt_objects)
-    print(len(txt_objects))
+    
+    for object_contents in objects['Contents']:
+        print('object_contents')
+        print(object_contents)
+        object_key = object_contents['Key']
+        object = s3_resource.Object(bucket_name, object_key)
+
+        object_data = object.get()["Body"].read()
+        print(object_data)
+
+        txt_objects.append(object_data)
+
+    print("TXT OBJECTS", len(txt_objects))
