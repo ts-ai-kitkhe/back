42a7974195c7a6a1b76b6c90c294368673c78658
CSopiko <skurd18@freeuni.edu.ge>
2022-08-27 19:24:54 +0400

    feat(ml): init concatenate text function

--- services/ml/functions/concatenate-text/handler.py
+++ services/ml/functions/concatenate-text/handler.py
@@ -1,2 +1,33 @@
-def hello(event, context):
+import os
+import boto3
+import re
+import json
+
+
+TXT_FOLDER_PATH = '/text/'
+ml_bucket_name = os.environ["S3_ML_BUCKET_NAME"]
+
+s3 = boto3.resource("s3")
+
+
+def main(event, context):
+    d = event.get("detail")
+    b = d.get("bucket")
+    bucket = b.get("name")
+    o = d.get("object")
+    key = o.get("key")
     print(event)
+
+    if not re.match(re.compile(r'\bbooks/.*/pages/text/.*\.txt\b'), key):
+        print(key)
+        return
+
+    if bucket != ml_bucket_name:
+        print(bucket)
+        return
+    
+    txt_objects = []
+    for obj in bucket.objects.all():
+        txt_objects.append(json.loads(obj.get()["Body"].read()))
+        print("*"*100, txt_objects)
+    print(len(txt_objects))
