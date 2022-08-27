051e56aef70c2f71600b8361c5c5bb74fe016084
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-08-27 21:31:48 +0400

    refactor(core): return pages by order in pages get all

--- services/core/functions/pages-get-all/handler.js
+++ services/core/functions/pages-get-all/handler.js
@@ -1,20 +1,32 @@
+import { BookPagesRepository } from "../../libs";
+
 var AWS = require("aws-sdk");
 var s3 = new AWS.S3();
 var bucketName = process.env.AWS_BUCKET_NAME;
 
 export async function main(event) {
   const bookId = event.pathParameters.id;
+  const bookPagesRepository = new BookPagesRepository();
+  const bookPages = await bookPagesRepository.get({ bookId: bookId });
+
+  const prefix = `books/${bookId}/pages/`;
   var params = {
     Bucket: bucketName,
-    Prefix: `books/${bookId}/pages/`,
+    Prefix: prefix,
   };
 
-  const res = await s3.listObjectsV2(params).promise();
+  const s3ListObjectsResult = await s3.listObjectsV2(params).promise();
+  const s3ObjectKeys = s3ListObjectsResult.Contents.map((elem) => elem.Key);
+  const pagesWithPrefix = bookPages.pages.map((page) => `${prefix}${page}`);
+  const filteredPages = pagesWithPrefix.filter((page) =>
+    s3ObjectKeys.includes(page)
+  );
+
   return {
     statusCode: 200,
     body: JSON.stringify(
-      res.Contents.map((elem) => ({
-        url: `https://assets.ts-ai-kitkhe.ge/${elem.Key}`,
+      filteredPages.map((elem) => ({
+        url: `https://assets.ts-ai-kitkhe.ge/${elem}`,
       }))
     ),
   };
